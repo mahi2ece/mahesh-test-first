0d859d950d222249bf28cfc1ecc0cdad84d79e02
shashank <muppirala.shashank@gmail.com>
2019-06-20 12:04:54 -0400

    hi

---
+++ src/main/java/com/example/mahesh/Test.java
@@ -0,0 +1,17 @@
+package com.example.mahesh;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+/**
+ * Test
+ */
+@Data
+@NoArgsConstructor
+public class Test {
+
+    @NonNull
+    private String data;
+    
+}
